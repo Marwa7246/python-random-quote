8dca000badee4e639a630b15f13d5f573b9dfd48
Marwa7246 <marwa.ragheb@gmail.com>
2020-11-14 11:11:19 -0500

    Append file programmaly

--- get-quote.py
+++ get-quote.py
@@ -3,13 +3,23 @@ import random
 def primary():
   # print("Keep it logically awesome.")
 
+  f = open("quotes.txt","a+")
+  for i in range(2):
+       f.write("Appended line %d\r\n" % (i+1))
+       
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last = 13
+  last = len(quotes) -1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  rnd2 = random.randint(0, last)
+
+  # print(quotes[last],quotes[rnd], end="")
+
+  tmp = random.sample(quotes, 2)
+  for i in tmp:
+    print(i, end="")
 
 if __name__== "__main__":
   primary()
